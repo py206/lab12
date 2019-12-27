53f54b91f1b4c71bbcedc3850f0ef54bfc0d8949
py206 <penny880206@gmail.com>
2019-12-27 16:21:24 +0800

    first commit

---
+++ app/src/main/java/com/example/lab12/Data.java
@@ -0,0 +1,16 @@
+package com.example.lab12;
+
+import javax.xml.transform.Result;
+
+public class Data {
+    Result result;
+
+    class Result{
+        Results[] results;
+
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
